adb9e4676dc69706aa4df41cded8da508149083d
Josep Valls <alfredvays@gmail.com>
2023-02-15 12:27:15 +0100

    Methods get post put and delete in users/service

--- src/components/auth/service.js
+++ src/components/auth/service.js
@@ -1,9 +1,34 @@
 import client from '../../api/client.js';
 
 const signupURL = '/api/users/signup';
+const usersURL = '/api/users';
+
+export const getAllUsers = async () => {
+  const response = await client.get(usersURL);
+
+  return response;
+};
+
+export const getUserById = async (userID) => {
+  const response = await client.get(`${usersURL}/${userID}`);
+
+  return response;
+};
 
 export const createUser = async (body) => {
-    const response = await client.post(signupURL, body);
-  
-    return response;
-  };
+  const response = await client.post(signupURL, body);
+
+  return response;
+};
+
+export const updateUser = async (userID, body) => {
+  const response = await client.put(`${usersURL}/${userID}`, body);
+
+  return response;
+};
+
+export const deleteUser = async (userID) => {
+  const response = await client.delete(`${usersURL}/${userID}`);
+
+  return response;
+};
