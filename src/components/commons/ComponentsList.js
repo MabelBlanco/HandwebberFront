2fac314d7927d7d2ad83f2c9391d323fbecb30be
Josep Valls <alfredvays@gmail.com>
2023-02-16 20:21:33 +0100

    test

--- src/components/commons/ComponentsList.js
+++ src/components/commons/ComponentsList.js
@@ -7,6 +7,7 @@ const ComponentsList = () => {
   return (
     <div className="row p-4 bg-light">
       <div className="row">
+        <h1>hola</h1>
         <h1 className="col col-12 text-center p-5">listado de componentes</h1>
         <Input label="Input label" className="col-md-6 pb-5" />
         <Select label="select" className="col-md-6 pb-5" />
