5618e02c3bf5620dd590942084d2760b180f36e7
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-02-16 16:49:50 +0100

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <Layout />
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/advertisements'
+          element={<Layout />}
+        />
+        <Route
+          path='/'
+          element={<Navigate to='/advertisements' />}
+        />
+      </Routes>
     </div>
   );
 }
