9ac2d8c82977c3c2ea84b24e028769b8d677f263
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-02-17 23:00:43 +0100

    Adslist done in cards list. Card modified to show fiels. Prepared an span to format fields titles 	modified:   src/components/advertisements/AdsList.js 	modified:   src/components/commons/card/Card.js

--- src/components/commons/card/Card.js
+++ src/components/commons/card/Card.js
@@ -1,4 +1,4 @@
-import NoImage from "../noImage/NoImage";
+import NoImage from '../noImage/NoImage';
 
 const Card = ({
   link_1,
@@ -7,31 +7,92 @@ const Card = ({
   name,
   price,
   tags,
+  stock,
+  idUser,
+  custom,
+  active,
   description,
   date,
   ...props
 }) => {
+  // console.log(tags);
+  // console.log(typeof tags);
   return (
-    <div className="card" style={{ width: "18rem" }} {...props}>
+    <div
+      className={`card ${active ? 'active' : ''}`}
+      style={{ width: '18rem' }}
+      {...props}
+    >
       {image ? (
-        <img src={image} className="card-img-top" alt="..." />
+        <img
+          src={image}
+          className='card-img-top'
+          alt='...'
+        />
       ) : (
-        <NoImage className="card-img-top" />
+        <NoImage className='card-img-top' />
       )}
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">{description}</p>
+      <div className='card-body'>
+        <h5 className='card-title'>{name}</h5>
+        <p className='card-text'>
+          <span>Description: </span>
+          {description}
+        </p>
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">{price}</li>
-        <li className="list-group-item">{tags}</li>
-        <li className="list-group-item">{date}</li>
+      <ul className='list-group list-group-flush'>
+        <li
+          key='price'
+          className='list-group-item'
+        >
+          <span>Price: </span>
+          {price}
+        </li>
+        <li
+          key='tags'
+          className='list-group-item'
+        >
+          <span>Tags: </span>
+          {tags.join(', ')}
+        </li>
+        <li
+          key='date'
+          className='list-group-item'
+        >
+          <span>Date: </span>
+          {date}
+        </li>
+        <li
+          key='stock'
+          className='list-group-item'
+        >
+          <span>Stock: </span>
+          {stock}
+        </li>
+        <li
+          key='user'
+          className='list-group-item'
+        >
+          <span>Usuario: </span>
+          {idUser}
+        </li>
+        <li
+          key='custom'
+          className='list-group-item'
+        >
+          {custom ? <span>'Custom Product'</span> : ''}
+        </li>
       </ul>
-      <div className="card-body">
-        <a href={link_1} className="card-link">
+      <div className='card-body'>
+        <a
+          href={link_1}
+          className='card-link'
+        >
           Card link
         </a>
-        <a href={link_2} className="card-link">
+        <a
+          href={link_2}
+          className='card-link'
+        >
           Another link
         </a>
       </div>
