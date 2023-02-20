8763b0785adfab2e92a036d45c22075235ece201
Josep Valls <alfredvays@gmail.com>
2023-02-20 12:29:14 +0100

    Funcionando Borrado de Usuario en ProfilePage.js  Changes to be committed: 	modified:   src/components/auth/signUp/ProfilePage.js

--- src/components/auth/signUp/ProfilePage.js
+++ src/components/auth/signUp/ProfilePage.js
@@ -1,9 +1,9 @@
-
 import NoImage from '../../commons/noImage/NoImage';
 import '../../commons/card/card.scss';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../commons/button/Button';
 import useDataUser from './useDataUser';
+import { deleteUser,  } from '../service';
 
 const initialState = {
   username: '',
@@ -11,23 +11,36 @@ const initialState = {
 };
 
 const ProfilePage = ({ className, title, ...props }) => {
-
   const { user, handleLogOut, isLogged } = useDataUser(initialState);
-  console.log(user)
+  console.log(user);
+
+  const navigate = useNavigate();
+
+  const deleteCount = async () => {
+    try {
+      const response = await deleteUser(user._id);
+      handleLogOut();
+      navigate('/');
+      return response;    
+    } catch (error) {
+      console.log(error);
+    };
+  };
 
   return (
     <div className='row'>
-      <div className='col-sm-6 py-5'>
-        <h1 className='col-sm-12 py-5'>{title}</h1>
-      </div>
-
       {isLogged && (
-        <div className='col-sm-6 py-5' style={{textAlign: 'right'}}>
+        <div className='col-sm-12 py-5' style={{ textAlign: 'center' }}>
           {' '}
+          <div className='card-body'>
+            <h2 className='card-title' style={{ fontSize: '50px' }}>
+              {user?.username}
+            </h2>
+          </div>{' '}
           <div className={'header-card'}>
             {user?.image ? (
               <img
-                style={{ height: '100px', width: '100px' }}
+                style={{ height: '300px', width: '300px' }}
                 src={`${process.env.REACT_APP_API_BASE_URL}/${user?.image}`}
                 className='card-img-top'
                 alt='...'
@@ -36,18 +49,39 @@ const ProfilePage = ({ className, title, ...props }) => {
               <NoImage className='card-img-top' />
             )}
           </div>
-          <div className='card-body'>
-            <h5 className='card-title'>{user?.username}</h5>
-          </div>
+          <ul className='list-group list-group-flush'>
+            <li key='mail' className='list-group-item'>
+              <span>Mail: </span>
+              {user?.mail}
+            </li>
+            <li key='subscriptions' className='list-group-item'>
+              <span>Favorites: </span>
+              <ul>
+              {user?.subscriptions && user.subscriptions.map( e => <li key={e}><Link to={`/advertisements/${e}`}>{e}</Link></li>)}
+              </ul>
+            </li>
+            {/*
+            <li key='date' className='list-group-item'>
+              <span>Date: </span>
+              {date}
+            </li>
+            <li key='stock' className='list-group-item'>
+              <span>Stock: </span>
+              {stock}
+            </li>
+            <li key='user' className='list-group-item'>
+              <span>Usuario: </span>
+              {idUser}
+            </li>
+            <li key='custom' className='list-group-item'>
+              {custom ? <span>'Custom Product'</span> : ''}
+            </li>*/}
+          </ul>
           <div className='card-body actions'>
             <Button
-              as={Link}
               type='button'
-              className='btn btn-secondary'
-              onClick={handleLogOut}
-            >
-              Logout
-            </Button>
+              onClick={deleteCount}
+            >DELETE COUNT</Button>
             <Button
               as={Link}
               to='/profile'
