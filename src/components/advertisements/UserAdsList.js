17d97e2941cbed5290a6d983ffed85eebb5208ed
Mabel <cimne7@gmail.com>
2023-02-24 19:05:46 +0100

    Translate UserAdsList

--- src/components/advertisements/UserAdsList.js
+++ src/components/advertisements/UserAdsList.js
@@ -1,63 +1,65 @@
 import useDataUser from "../auth/signUp/useDataUser";
 import NoImage from "../commons/noImage/NoImage";
-import '../commons/card/card.scss';
+import "../commons/card/card.scss";
 import Card from "../commons/card/Card";
+import { useTranslation } from "react-i18next";
 
 const initialState = {
-  username: '',
-  mail: '',
-  password: '',
-  image: '',
-  ads: []
+  username: "",
+  mail: "",
+  password: "",
+  image: "",
+  ads: [],
 };
 
-const UserAdsList = ({...props}) => {
+const UserAdsList = ({ ...props }) => {
   const { userSearch } = useDataUser(initialState);
+  const { t } = useTranslation();
 
   const advertisements = userSearch?.ads;
 
-    return (
-      <div className='row'>
-        <div className='col-sm-12 py-5' style={{ textAlign: 'center' }}>
-          {' '}
-          <div className='card-body'>
-            <h2 className='card-title' style={{ fontSize: '50px' }}>
-              {userSearch?.username}
-            </h2>
-          </div>{' '}
-          <div className={'header-card'}>
-            {userSearch?.image ? (
-              <img
-                style={{ height: '300px', width: '300px' }}
-                src={`${process.env.REACT_APP_API_BASE_URL}/${userSearch?.image}`}
-                className='card-img-top'
-                alt='...'
-              />
-            ) : (
-              <NoImage className='card-img-top' />
-            )}
+  return (
+    <div className="row">
+      <div className="col-sm-12 py-5" style={{ textAlign: "center" }}>
+        {" "}
+        <div className="card-body">
+          <h2 className="card-title" style={{ fontSize: "50px" }}>
+            {userSearch?.username}
+          </h2>
+        </div>{" "}
+        <div className={"header-card"}>
+          {userSearch?.image ? (
+            <img
+              style={{ height: "300px", width: "300px" }}
+              src={`${process.env.REACT_APP_API_BASE_URL}/${userSearch?.image}`}
+              className="card-img-top"
+              alt="..."
+            />
+          ) : (
+            <NoImage className="card-img-top" />
+          )}
         </div>
-        <ul className='list-group list-group-flush'>
-            <li key='mail' className='list-group-item'>
-              <span>Mail: </span>
-              {userSearch?.mail}
-            </li>
-      </ul>
-      {advertisements?.map((element) => {
-        const newProps = { ...props, ...element };
-        return (
-          <Card
-            className="col-sm-12 col-lg-3 m-2"
-            key={element._id}
-            {...newProps}
-            link_1={`/advertisements/${element._id}`}
-            label_link_1="See more"
-          />
-        );
-      })}
+        <ul className="list-group list-group-flush">
+          <li key="mail" className="list-group-item">
+            <span>{t("UserAdsList.Mail")}: </span>
+            {userSearch?.mail}
+          </li>
+        </ul>
+        {advertisements?.map((element) => {
+          const newProps = { ...props, ...element };
+          return (
+            <Card
+              className="col-sm-12 col-lg-3 m-2"
+              key={element._id}
+              {...newProps}
+              link_1={`/advertisements/${element._id}`}
+              label_link_1={t("UserAdsList.See more")}
+            />
+          );
+        })}
       </div>
-      </div>
-    )
+    </div>
+  );
 };
 
 export default UserAdsList;
