da7514c57115f3aad6be931d769d5e22e1b69c63
Mabel <cimne7@gmail.com>
2023-02-25 15:47:21 +0100

    Translate NoImage

--- src/components/commons/noImage/NoImage.js
+++ src/components/commons/noImage/NoImage.js
@@ -1,9 +1,11 @@
 import classNames from "classnames";
 import "./noImage.scss";
+import { useTranslation } from "react-i18next";
 const NoImage = ({ className, ...props }) => {
+  const { t } = useTranslation();
   return (
     <div className={classNames("noImage", className)} {...props}>
-      No image
+      {t("NoImage.No image")}
     </div>
   );
 };
