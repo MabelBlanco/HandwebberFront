ef0b1d71c7b377c37db87d25652c403fc4e36617
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-03-15 17:30:33 +0100

    Ask user by username. Cleaning console.log().
    	modificado:     src/App.js
    	modificado:     src/components/advertisements/UserAdsList.js
    	modificado:     src/components/auth/RequireAuth.js
    	modificado:     src/components/auth/signUp/ProfilePage.js
    	modificado:     src/components/auth/signUp/UserInfo.js
    	modificado:     src/components/auth/signUp/useDataUser.js
    	modificado:     src/components/commons/card/Card.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
-import AdsList from "./components/advertisements/AdsList";
-import DetailAdvertisement from "./components/advertisements/DetailAdvertisement";
-import EditAveritisement from "./components/advertisements/EditAdvertisement";
-import NewAdvertisement from "./components/advertisements/NewAdvertisement";
-import UserAdsList from "./components/advertisements/UserAdsList";
-import { LoginPage } from "./components/auth/login/LoginPage";
-import RequireAuth from "./components/auth/RequireAuth";
-import ProfilePage from "./components/auth/signUp/ProfilePage";
-import SignUp from "./components/auth/signUp/SignUp";
-import { Chat } from "./components/Chat/chat/Chat.js";
-import NotFoundPage from "./components/commons/feedbacks/NotFound/NotFoundPage";
-import Layout from "./components/Layout/Layout";
-import LayoutTest from "./components/Layout/LayoutTest";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import AdsList from './components/advertisements/AdsList';
+import DetailAdvertisement from './components/advertisements/DetailAdvertisement';
+import EditAveritisement from './components/advertisements/EditAdvertisement';
+import NewAdvertisement from './components/advertisements/NewAdvertisement';
+import UserAdsList from './components/advertisements/UserAdsList';
+import { LoginPage } from './components/auth/login/LoginPage';
+import RequireAuth from './components/auth/RequireAuth';
+import ProfilePage from './components/auth/signUp/ProfilePage';
+import SignUp from './components/auth/signUp/SignUp';
+import { Chat } from './components/Chat/chat/Chat.js';
+import NotFoundPage from './components/commons/feedbacks/NotFound/NotFoundPage';
+import Layout from './components/Layout/Layout';
+import LayoutTest from './components/Layout/LayoutTest';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/login" element={<Layout title="Login" />}>
-          <Route path="" element={<LoginPage />} />
+        <Route
+          path='/login'
+          element={<Layout title='Login' />}>
+          <Route
+            path=''
+            element={<LoginPage />}
+          />
         </Route>
 
-        <Route path="/signup" element={<Layout title="Sign Up" />}>
-          <Route path="" element={<SignUp />} />
+        <Route
+          path='/signup'
+          element={<Layout title='Sign Up' />}>
+          <Route
+            path=''
+            element={<SignUp />}
+          />
         </Route>
 
-        <Route path="/profile" element={<Layout title="Profile" />}>
+        <Route
+          path='/profile'
+          element={<Layout title='Profile' />}>
           <Route
             index
             element={
@@ -35,46 +47,68 @@ function App() {
               </RequireAuth>
             }
           />
-          <Route path="user/:idUser" element={<UserAdsList />} />
+          <Route
+            path='user/:username'
+            element={<UserAdsList />}
+          />
         </Route>
-
-        <Route path="chat" element={<Chat />} />
-        <Route path="/" element={<Navigate to="/advertisements" />} />
 
         <Route
-          path="/advertisements"
-          element={<Layout title="Advertisments" />}
-        >
-          <Route index element={<AdsList title="Advertisement List" />} />
+          path='chat'
+          element={<Chat />}
+        />
+        <Route
+          path='/'
+          element={<Navigate to='/advertisements' />}
+        />
+
+        <Route
+          path='/advertisements'
+          element={<Layout title='Advertisments' />}>
           <Route
-            path=":id"
-            element={<DetailAdvertisement title="Advertisement Detail" />}
+            index
+            element={<AdsList title='Advertisement List' />}
           />
           <Route
-            path="edit/:id"
+            path=':id'
+            element={<DetailAdvertisement title='Advertisement Detail' />}
+          />
+          <Route
+            path='edit/:id'
             element={
               <RequireAuth>
-                <EditAveritisement title="Editing Avertisement" />
+                <EditAveritisement title='Editing Avertisement' />
               </RequireAuth>
             }
           />
           <Route
-            path="new"
+            path='new'
             element={
               <RequireAuth>
-                <NewAdvertisement title="New Advertisement" />
+                <NewAdvertisement title='New Advertisement' />
               </RequireAuth>
             }
           />
         </Route>
 
-        <Route path="/test" element={<LayoutTest />} />
+        <Route
+          path='/test'
+          element={<LayoutTest />}
+        />
 
-        <Route path="/404" element={<Layout title="NOT FOUND" />}>
-          <Route path="" element={<NotFoundPage />} />
+        <Route
+          path='/404'
+          element={<Layout title='NOT FOUND' />}>
+          <Route
+            path=''
+            element={<NotFoundPage />}
+          />
         </Route>
 
-        <Route path="*" element={<Navigate to="/404" />} />
+        <Route
+          path='*'
+          element={<Navigate to='/404' />}
+        />
       </Routes>
     </div>
   );
