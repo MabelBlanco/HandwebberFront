a25973d42c6a9091f7aef96767b047d16fbf872d
Rutthy52 <rutthy52@Rutthy52s-MacBook-Pro.local>
2023-03-16 12:31:06 +0100

    styles tablet footer, header and loginPage

--- src/components/Layout/footer/Footer.js
+++ src/components/Layout/footer/Footer.js
@@ -5,32 +5,29 @@ import "./Footer.scss";
 const Footer = ({ className, ...props }) => {
   return (
     <footer
-      className={classNames("text-center text-white ", className)}
+      className={classNames(
+        "text-white d-flex flex-md-row justify-content-between align-items-center px-md-5 py-2 mx-0",
+        className
+      )}
       {...props}
     >
-      <div className="container d-flex justify-content-between align-items-center py-3">
-        <section>
-          <Link className="btn btn-floating m-1 " to="!#" role="button">
-            <i className="fab fa-facebook-f"></i>
-          </Link>
-          <Link className="btn  btn-floating m-1 " to="!#" role="button">
-            <i className="fab fa-twitter"></i>
-          </Link>
-          <Link className="btn btn-floating m-1 " to="!#" role="button">
-            <i className="bi bi-youtube"></i>
-          </Link>
-          <Link className="btn btn-floating m-1 " to="!#" role="button">
-            <i className="bi bi-facebook"></i>
-          </Link>
-          <Link className="btn btn-floating m-1 " to="!#" role="button">
-            <i className="bi bi-linkedin"></i>
-          </Link>
-          <Link className="btn  m-1 " to="!#" role="button">
-            <i className="bi bi-twitter"></i>
-          </Link>
-        </section>
-        <section>© 2023 Copyright handWebber</section>
-      </div>
+      <section className="col-xs-12 col-md-6 text-center">
+        <Link className="btn btn-floating  " to="!#" role="button">
+          <i className="bi bi-youtube"></i>
+        </Link>
+        <Link className="btn btn-floating " to="!#" role="button">
+          <i className="bi bi-facebook"></i>
+        </Link>
+        <Link className="btn btn-floating " to="!#" role="button">
+          <i className="bi bi-linkedin"></i>
+        </Link>
+        <Link className="btn" to="!#" role="button">
+          <i className="bi bi-twitter"></i>
+        </Link>
+      </section>
+      <section className="col-xs-12 col-md-6 text-center">
+        © 2023 Copyright handWebber
+      </section>
     </footer>
   );
 };
