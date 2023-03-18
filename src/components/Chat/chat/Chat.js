e8fa6b48e780d50ddcf759e1cea1ff7dbaf4e4a8
Mabel <cimne7@gmail.com>
2023-03-18 17:38:00 +0100

    init chat styles

--- src/components/Chat/chat/Chat.js
+++ src/components/Chat/chat/Chat.js
@@ -5,8 +5,11 @@ import { socket } from "../../..";
 import { useIsLoggedSelector } from "../../../store/authSlice";
 import { getAdvertisementDetail } from "../../advertisements/service";
 import { getUserById } from "../../auth/service";
+import NoImage from "../../commons/noImage/NoImage";
 import { Conversation } from "../conversation/conversation";
 import { getConversations } from "../conversation/service";
+
+import "./chat.css";
 
 export function Chat() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -34,12 +37,15 @@ export function Chat() {
           );
           const advertisementNameConversation =
             advertisementConversation.result.name;
+          const advertisementImageConversation =
+            advertisementConversation.result.image;
 
           const userNameTo = conver.users.filter((data) => data !== user._id);
           const userConversation = await getUserById(userNameTo[0]);
           const userNameConversation = userConversation.result.username;
 
           conver.advertisementName = advertisementNameConversation;
+          conver.advertisementImage = advertisementImageConversation;
           conver.userToId = userNameTo[0];
           conver.userToName = userNameConversation;
 
@@ -92,12 +98,15 @@ export function Chat() {
       );
       const advertisementNameConversation =
         advertisementConversation.result.name;
+      const advertisementImageConversation =
+        advertisementConversation.result.image;
 
       const userNameTo = conver.users.filter((data) => data !== user._id);
       const userConversation = await getUserById(userNameTo[0]);
       const userNameConversation = userConversation.result.username;
 
       conver.advertisementName = advertisementNameConversation;
+      conver.advertisementImage = advertisementImageConversation;
       conver.userToId = userNameTo[0];
       conver.userToName = userNameConversation;
 
@@ -125,32 +134,54 @@ export function Chat() {
           userToComplete={userToComplete}
         ></Conversation>
       ) : (
-        <div>
-          {conversations.length ? (
-            <ul>
-              {conversations.map((conversation) => {
-                return (
-                  <li key={conversation._id}>
-                    <Link
-                      to={`/chat?ad_id=${conversation.advertisement}&user_id=${conversation.userToId}`}
-                    >
-                      <p key={conversation.advertisementName}>
-                        {conversation.advertisementName}
-                      </p>
-                      <p key={conversation.userToName}>
-                        {conversation.userToName}
-                      </p>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          ) : (
-            <p>
-              Aún no tienes conversaciones. ¡Empieza una y disfruta de nuestro
-              chat!
-            </p>
-          )}
+        <div className="container-fluid h-100">
+          <div className="row justify-content-center h-100">
+            <div className="col-md-4 col-xl-3 chat">
+              <div className="card mb-sm-3 mb-md-0 conversations_card">
+                <div className="card-body conversations_body">
+                  {conversations.length ? (
+                    <ul className="conversations">
+                      {conversations.map((conversation) => {
+                        return (
+                          <li key={conversation._id}>
+                            <Link
+                              to={`/chat?ad_id=${conversation.advertisement}&user_id=${conversation.userToId}`}
+                              className="d-flex bd-highlight"
+                            >
+                              <div className="img_cont">
+                                {conversation.advertisementImage ? (
+                                  <img
+                                    src={`${process.env.REACT_APP_API_BASE_URL}/${conversation.advertisementImage}`}
+                                    className="rounded-circle advert_img"
+                                    alt={conversation.advertisementName}
+                                  />
+                                ) : (
+                                  <NoImage className="rounded-circle advert_img" />
+                                )}
+                              </div>
+                              <div className="conver_info">
+                                <span key={conversation.advertisementName}>
+                                  {conversation.advertisementName}
+                                </span>
+                                <p key={conversation.userToName}>
+                                  {conversation.userToName}
+                                </p>
+                              </div>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  ) : (
+                    <p>
+                      Aún no tienes conversaciones. ¡Empieza una y disfruta de
+                      nuestro chat!
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
