7e51c87ecb2a97e5eedc3c93396506ad0cdc544d
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-03-21 16:12:29 +0100

    detailAd user now calling user and no id

--- src/components/advertisements/AdsDetailPage/AdsDetailPage.js
+++ src/components/advertisements/AdsDetailPage/AdsDetailPage.js
@@ -1,18 +1,18 @@
-import classNames from "classnames";
-import { formatDistanceToNow } from "date-fns";
-import { t } from "i18next";
-import { NavLink } from "react-router-dom";
+import classNames from 'classnames';
+import { formatDistanceToNow } from 'date-fns';
+import { t } from 'i18next';
+import { NavLink } from 'react-router-dom';
 import {
   FacebookIcon,
   FacebookShareButton,
   LinkedinIcon,
   LinkedinShareButton,
-} from "react-share";
-import Button from "../../commons/button/Button";
-import Modal from "../../commons/modal/Modal";
-import NoImage from "../../commons/noImage/NoImage";
-import Tags from "../../commons/tags/Tags";
-import "./adsDetailPage.scss";
+} from 'react-share';
+import Button from '../../commons/button/Button';
+import Modal from '../../commons/modal/Modal';
+import NoImage from '../../commons/noImage/NoImage';
+import Tags from '../../commons/tags/Tags';
+import './adsDetailPage.scss';
 
 const AdsDetailPage = ({
   className,
@@ -41,127 +41,146 @@ const AdsDetailPage = ({
   return (
     <section
       className={classNames(
-        "py-5 ads-detail-page blur-secondary-800",
+        'py-5 ads-detail-page blur-secondary-800',
         className
       )}
-      {...props}
-    >
-      <div className="container px-4 px-lg-5 my-5">
-        <div className="row gx-4 gx-lg-5 ">
-          <div className="col-md-6 image">
+      {...props}>
+      <div className='container px-4 px-lg-5 my-5'>
+        <div className='row gx-4 gx-lg-5 '>
+          <div className='col-md-6 image'>
             {image ? (
               <img
                 src={`${process.env.REACT_APP_API_BASE_URL}/${image}`}
-                className="card-img-top"
-                alt="..."
+                className='card-img-top'
+                alt='...'
               />
             ) : (
               <NoImage />
             )}
           </div>
-          <div className="col-md-6 ads-info">
-            <h1 className="display-5 fw-bolder name" key="name">
+          <div className='col-md-6 ads-info'>
+            <h1
+              className='display-5 fw-bolder name'
+              key='name'>
               {name}
             </h1>
-            <div key="date" className="date">
-              <span className="label-info">Date: </span>
+            <div
+              key='date'
+              className='date'>
+              <span className='label-info'>Date: </span>
               <time dateTime={update}>
-                <small className="text-muted">
-                  {formatDistanceToNow(new Date(update), "MM/dd/yyy")}
+                <small className='text-muted'>
+                  {formatDistanceToNow(new Date(update), 'MM/dd/yyy')}
                 </small>
               </time>
             </div>
-            <div className="fs-5 mb-5 price">
-              <span key="price" className="label-info">
-                {t("AdsDetailPage.Price")}:
+            <div className='fs-5 mb-5 price'>
+              <span
+                key='price'
+                className='label-info'>
+                {t('AdsDetailPage.Price')}:
               </span>
               <span> {price}€</span>
             </div>
-            <div className="lead description" key="description">
-              <p className="label-info">{t("AdsDetailPage.Description")}:</p>
+            <div
+              className='lead description'
+              key='description'>
+              <p className='label-info'>{t('AdsDetailPage.Description')}:</p>
               <p>{description}</p>
             </div>
-            <div key="tags" className="list-group-item tags-container">
-              <p className="label-info">Tags: </p>
+            <div
+              key='tags'
+              className='list-group-item tags-container'>
+              <p className='label-info'>Tags: </p>
               <Tags tagsArray={tags} />
             </div>
-            <div key="stock" className="list-group-item stock">
-              <span className="label-info">Stock: </span>
+            <div
+              key='stock'
+              className='list-group-item stock'>
+              <span className='label-info'>Stock: </span>
               {stock}
             </div>
-            <div key="active" className="list-group-item active">
+            <div
+              key='active'
+              className='list-group-item active'>
               {active ? t(`AdsDetailPage.Active`) : t(`AdsDetailPage.Inactive`)}
             </div>
-            <div key="user" className="list-group-item user">
-              <span className="label-info">Usuario: </span>
+            <div
+              key='user'
+              className='list-group-item user'>
+              <span className='label-info'>Usuario: </span>
               <NavLink
-                to={`/profile/user/${idUser._id}`}
-                className="card-link"
-                end
-              >
+                to={`/profile/user/${idUser.username}`}
+                className='card-link'
+                end>
                 {idUser.username}
               </NavLink>
             </div>
             {custom && (
-              <div key="custom" className="list-group-item custom">
+              <div
+                key='custom'
+                className='list-group-item custom'>
                 <span>Custom Product</span>
               </div>
             )}
             {children}
             {idUser._id === userLoggedId && (
-              <div className="mt-5 actions">
+              <div className='mt-5 actions'>
                 <Button
-                  type="button"
-                  className="btn btn-secondary blur-secondary-800 radius-2  "
-                  onClick={fnedit}
-                >
+                  type='button'
+                  className='btn btn-secondary blur-secondary-800 radius-2  '
+                  onClick={fnedit}>
                   {t(`AdsDetailPage.Edit`)}
                 </Button>
                 <Modal
                   hasConfirm
                   modalTitle={t(`AdsDetailPage.ModalTitle`)}
                   doTask={fndelete}
-                  classNameBtn="ms-2 btn-secondary"
-                  classNameBtnClose="btn-secondary"
-                  classNameBtnConfirm="btn-primary"
-                  classNameContent="body"
+                  classNameBtn='ms-2 btn-secondary'
+                  classNameBtnClose='btn-secondary'
+                  classNameBtnConfirm='btn-primary'
+                  classNameContent='body'
                   label_confirm={t(`AdsDetailPage.Delete`)}
                   label_cancel={t(`AdsDetailPage.Cancel`)}
                   label_btn={t(`AdsDetailPage.Delete`)}
-                  modalId="deleteAdvert"
-                >
+                  modalId='deleteAdvert'>
                   {t(`AdsDetailPage.ModalText`)}
                 </Modal>
               </div>
             )}
             {userLoggedId && idUser._id !== userLoggedId && (
-              <div className="mt-5 actions">
+              <div className='mt-5 actions'>
                 <Button
-                  type="button"
-                  className="btn btn-secondary blur-secondary-800 radius-2 ms-3"
-                  onClick={fncontact}
-                >
+                  type='button'
+                  className='btn btn-secondary blur-secondary-800 radius-2 ms-3'
+                  onClick={fncontact}>
                   {t(`AdsDetailPage.Contact`)}
                 </Button>
               </div>
             )}
           </div>
-          <div className="mt-5 d-flex align-items-center justify-content-center gap-3">
+          <div className='mt-5 d-flex align-items-center justify-content-center gap-3'>
             <FacebookShareButton
               url={`${process.env.REACT_APP_DOMAIN_URL}/advertisements/${advert._id}-${advert.name}`}
-              className="Demo__some-network__share-button"
-              quote="HandWebber"
-              hashtag={`#${advert.name}`}
-            >
-              <FacebookIcon size={24} round /> {t(`AdsDetailPage.Share`)}
+              className='Demo__some-network__share-button'
+              quote='HandWebber'
+              hashtag={`#${advert.name}`}>
+              <FacebookIcon
+                size={24}
+                round
+              />{' '}
+              {t(`AdsDetailPage.Share`)}
             </FacebookShareButton>
             <LinkedinShareButton
               url={`${process.env.REACT_APP_DOMAIN_URL}/advertisements/${advert._id}-${advert.name}`}
               title={`Info about Advertisement ${advert.name}`}
               summary={`DESCRIPTION: ${advert.description}`}
-              quote={`HandWebber`}
-            >
-              <LinkedinIcon size={24} round /> {t(`AdsDetailPage.Share`)}
+              quote={`HandWebber`}>
+              <LinkedinIcon
+                size={24}
+                round
+              />{' '}
+              {t(`AdsDetailPage.Share`)}
             </LinkedinShareButton>
           </div>
         </div>
