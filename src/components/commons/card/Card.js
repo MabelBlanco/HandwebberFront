30086169eb6d0d46527ab8a33bb3f4674b3eb0cc
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2023-03-22 17:01:41 +0100

    quitada opacity de Card.js

--- src/components/commons/card/Card.js
+++ src/components/commons/card/Card.js
@@ -1,11 +1,11 @@
-import classNames from "classnames";
-import { formatDistanceToNow } from "date-fns";
-import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
-import Button from "../button/Button";
-import NoImage from "../noImage/NoImage";
-import Tags from "../tags/Tags";
-import "./card.scss";
+import classNames from 'classnames';
+import { formatDistanceToNow } from 'date-fns';
+import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+import Button from '../button/Button';
+import NoImage from '../noImage/NoImage';
+import Tags from '../tags/Tags';
+import './card.scss';
 
 /**
  *
@@ -45,88 +45,106 @@ const Card = ({
 
   return (
     <div
-      className={classNames(`card ${active ? "active" : ""}`, className)}
-      {...props}
-    >
+      className={classNames(`card ${active ? 'active' : ''}`, className)}
+      {...props}>
       <div
-        className="header-card"
+        className='header-card'
         style={{
           backgroundImage: `url(${process.env.REACT_APP_API_BASE_URL}/${image})`,
-        }}
-      >
+        }}>
         {image ? (
           <img
             src={`${process.env.REACT_APP_API_BASE_URL}/${image}`}
-            className="card-img-top"
-            alt="..."
-            style={{ opacity: 0 }}
+            className='card-img-top'
+            alt='...'
           />
         ) : (
-          <NoImage className="card-img-top" />
+          <NoImage className='card-img-top' />
         )}
       </div>
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">
-          <span>{t("Card.Description")}: </span>
+      <div className='card-body'>
+        <h5 className='card-title'>{name}</h5>
+        <p className='card-text'>
+          <span>{t('Card.Description')}: </span>
           {description}
         </p>
       </div>
-      <ul className="list-group list-group-flush">
-        <li key="price" className="list-group-item">
-          <span>{t("Card.Price")}: </span>
+      <ul className='list-group list-group-flush'>
+        <li
+          key='price'
+          className='list-group-item'>
+          <span>{t('Card.Price')}: </span>
           {price}
         </li>
-        <li key="tags" className="list-group-item tags-container">
-          <p className="label-info">Tags: </p>
+        <li
+          key='tags'
+          className='list-group-item tags-container'>
+          <p className='label-info'>Tags: </p>
           <Tags tagsArray={tags} />
         </li>
         {
           // If it's necessary to show the date, put true
           date && (
-            <li key="date" className="list-group-item">
-              <span>{t("Card.Date")}: </span>
+            <li
+              key='date'
+              className='list-group-item'>
+              <span>{t('Card.Date')}: </span>
               <time dateTime={date}>
-                <small className="text-muted">
-                  {formatDistanceToNow(new Date(date), "MM/dd/yyy")}
+                <small className='text-muted'>
+                  {formatDistanceToNow(new Date(date), 'MM/dd/yyy')}
                 </small>
               </time>
             </li>
           )
         }
-        <li key="stock" className="list-group-item">
-          <span>{t("Card.Stock")}: </span>
+        <li
+          key='stock'
+          className='list-group-item'>
+          <span>{t('Card.Stock')}: </span>
           {stock}
-        </li>{" "}
-        <li key="favorites" className="list-group-item favorites">
-          <i className="bi bi-heart-fill" onClick={addFavorites}></i>{" "}
-          <span className="px-1">{subscribers && subscribers.length}</span>
+        </li>{' '}
+        <li
+          key='favorites'
+          className='list-group-item favorites'>
+          <i
+            className='bi bi-heart-fill'
+            onClick={addFavorites}></i>{' '}
+          <span className='px-1'>{subscribers && subscribers.length}</span>
         </li>
-        <li key="user" className="list-group-item">
-          <span>{t("Card.User")}: </span>
+        <li
+          key='user'
+          className='list-group-item'>
+          <span>{t('Card.User')}: </span>
           <NavLink
             to={`/profile/user/${idUser.username}`}
-            className="card-link"
-            end
-          >
+            className='card-link'
+            end>
             {idUser.username}
           </NavLink>
         </li>
-        <li key="custom" className="list-group-item">
-          {custom ? <span> {t("Card.Custom Product")} </span> : ""}
+        <li
+          key='custom'
+          className='list-group-item'>
+          {custom ? <span> {t('Card.Custom Product')} </span> : ''}
         </li>
       </ul>
-      <div className="card-body actions">
-        <NavLink to={link_1} className="card-link">
+      <div className='card-body actions'>
+        <NavLink
+          to={link_1}
+          className='card-link'>
           {label_link_1}
         </NavLink>
         {label_button_1 && (
-          <Button type="button" className="btn btn-secondary mx-3">
+          <Button
+            type='button'
+            className='btn btn-secondary mx-3'>
             {label_button_1}
           </Button>
         )}
         {label_button_2 && (
-          <Button type="button" className="btn btn-secondary">
+          <Button
+            type='button'
+            className='btn btn-secondary'>
             {label_button_2}
           </Button>
         )}
